b83e51ab04cfb65eed4a3178565e012ff31561eb
OutOfBoozeException <outofboozeexception@gmail.com>
2019-11-09 15:45:19 +0100

    pred front-end

--- application/controllers/Pages.php
+++ application/controllers/Pages.php
@@ -1,12 +1,19 @@
 <?php
 class Pages extends CI_Controller{
-	public function view($page='home'){
+	public function view($page='index'){
 			if (!file_exists(APPPATH.'views/pages/'.$page .'.php')){
 				show_404();
 			}
-			$data['title']=ucfirst($page);
-			$this->load->view('templates/header');
-			$this->load->view('pages/'.$page,$data);
-			$this->load->view('templates/footer');
+			
+			if ($page != 'index') {
+				$data['title']=ucfirst($page);
+				$this->load->view('templates/header');
+				$this->load->view('pages/'.$page,$data);
+				$this->load->view('templates/footer');
+			}
+			else {
+				$data['title']=ucfirst($page);
+				$this->load->view('pages/'.$page,$data);
+			}
 	}
 }
