e366c90aca882b8ba0e9bb29ccd181170d03680f
share95 <syndra11@gmail.com>
2019-11-09 19:28:44 +0100

    adding Model and some controllers and views

--- application/config/routes.php
+++ application/config/routes.php
@@ -2,7 +2,10 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 #$route['users']='users/index';
+
 $route['default_controller'] = 'pages/view';
+$route['leaf_upload']='leaf_upload/view';
+$route['cure/(:any)']='cure/view/$1';
 $route['randoms']='randoms/view';
 $route['(:any)']='pages/view/$1';
 $route['404_override'] = '';
